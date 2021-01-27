b3bd0d23572ce429ec64bb64617ad4ef5c2d379b
Andrew McGhie <andrewcmcghie@gmail.com>
2021-01-26 19:04:33 -0500

    fixed api.js

---
+++ routes/api/user.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const userController = require('../../controllers/userController');
+
+// Matches with "/api/user"
+router.route('/').get(userController.findAll).post(postController.create);
+
+// Matches with "/api/user/:id"
+router
+  .route('/:id')
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
+
+module.exports = router;
