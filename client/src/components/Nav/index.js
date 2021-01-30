ddaa28fa88eb163c2bb38b1d80b52ee21b664cf7
Andrew McGhie <andrewcmcghie@gmail.com>
2021-01-30 14:35:08 -0500

    Signup Page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,23 +1,39 @@
-import React from "react";
-import {Navbar, Nav} from "react-bootstrap";
-import "./style.css";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import './style.css';
 
 function Navigate() {
-
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand className="title" href="/"><h1><span>jam</span><br /><span>Hammer</span></h1></Navbar.Brand>
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand className="title" href="/">
+        <h1>
+          <span>jam</span>
+          <br />
+          <span>Hammer</span>
+        </h1>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link className="nav-links" href="/recording"><h3>Record</h3></Nav.Link>
-          <Nav.Link className="nav-links" href="/upload"><h3>Upload</h3></Nav.Link>
-          <Nav.Link className="nav-links" href="/search"><h3>Search</h3></Nav.Link>
-          <Nav.Link className="nav-links" href="/user"><h3>Profile</h3></Nav.Link>
+          <Nav.Link className="nav-links" href="/register">
+            <h3>Signup</h3>
+          </Nav.Link>
+          <Nav.Link className="nav-links" href="/recording">
+            <h3>Record</h3>
+          </Nav.Link>
+          <Nav.Link className="nav-links" href="/upload">
+            <h3>Upload</h3>
+          </Nav.Link>
+          <Nav.Link className="nav-links" href="/search">
+            <h3>Search</h3>
+          </Nav.Link>
+          <Nav.Link className="nav-links" href="/user">
+            <h3>Profile</h3>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-    );
-  }
+  );
+}
 
 export default Navigate;
