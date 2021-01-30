a74c09df71bb59aefbb06e60b329e19e9a387cdc
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-01-30 12:38:36 -0500

    added login and registration

--- models/users.js
+++ models/users.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+var validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const User = new Schema({
-    username : {type: String, unique: true, required:true}
+    realName: String,
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    profilePicture: {
+        type:String,
+        default: "b0383d76-a9c6-482e-80a1-38bc4bc772ec"
+    } 
 });
 
 User.plugin(passportLocalMongoose);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const songRoutes = require("./songs");
 const userRoutes = require("./user");
+const loginRoute = require("./login");
+const registerRoute = require("./register");
 
 // Book routes
 router.use("/song", songRoutes);
 router.use("/users", userRoutes);
+router.use("/login", loginRoute);
+router.use("/register", registerRoute);
 
 module.exports = router;
