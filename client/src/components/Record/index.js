c34f5b908a94eefea7b41c7cc7e96c6f6acdaae4
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-01-30 13:52:01 -0500

    Changed from react-media-recorder to videojs-record

--- client/src/components/Record/index.js
+++ client/src/components/Record/index.js
@@ -1,62 +1,93 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { Col, Row, Container } from '../Grid';
-import { useReactMediaRecorder } from "react-media-recorder";
 import axios from "axios";
 import Jumbotron from '../Jumbotron';
 import "./style.css";
 
-function Record() {
+import 'video.js/dist/video-js.css';
+import videojs from 'video.js';
 
-        const {
-          status,
-          startRecording,
-          stopRecording,
-          mediaBlobUrl,
-          mediaRecorder
-        } = useReactMediaRecorder({ audio: true });
+import 'webrtc-adapter';
+import RecordRTC from 'recordrtc';
 
-        
-        
-        async function Upload(ev){
-            let blob = await fetch(mediaBlobUrl).then(r => r.blob());
-            var fd = new FormData();
-            fd.append('fname', 'MySong.wav');
-            fd.append('data', blob);
-            axios({
-                method: 'post',
-                url: '/api/song',
-                data: fd,
-                processData: false,
-                contentType: false
-            }).then(function (data) {
-                console.log(data);
-            });
-        }
 
-    return (
+// Required imports when recording audio-only using the videojs-wavesurfer plugin
+import WaveSurfer from 'wavesurfer.js';
+import MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone.js';
+// Register videojs-wavesurfer plugin
+import 'videojs-wavesurfer/dist/css/videojs.wavesurfer.css';
+import Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
+
+
+// register videojs-record plugin with this import
+import 'videojs-record/dist/css/videojs.record.css';
+import Record from 'videojs-record/dist/videojs.record.js';
+
+WaveSurfer.microphone = MicrophonePlugin;
+
+
+class RecordComp extends Component {
+    
+
+    componentDidMount() {
+        // instantiate Video.js
+        this.player = videojs(this.videoNode, this.props, () => {
+            // print version information at startup
+            const version_info = 'Using video.js ' + videojs.VERSION +
+                ' with videojs-record ' + videojs.getPluginVersion('record') +
+                ' and recordrtc ' + RecordRTC.version;
+            videojs.log(version_info);
+        });
+
+        // device is ready
+        this.player.on('deviceReady', () => {
+            console.log('device is ready!');
+        });
+
+        // user clicked the record button and started recording
+        this.player.on('startRecord', () => {
+            console.log('started recording!');
+        });
+
+        // user completed recording and stream is available
+        this.player.on('finishRecord', () => {
+            // recordedData is a blob object containing the recorded data that
+            // can be downloaded by the user, stored on server etc.
+            console.log('finished recording: ', this.player.recordedData);
+        });
+
+        // error handling
+        this.player.on('error', (element, error) => {
+            console.warn(error);
+        });
+
+        this.player.on('deviceError', () => {
+            console.error('device error:', this.player.deviceErrorCode);
+        });
+    }
+
+    render(){
+        return (
         <Container fluid>
             <Row>
                 <Col size="md-12">
                     <Jumbotron>
                         <Row>
                             <div className="col-md-12">
-                                <p>{status}</p>
-                                <audio src={mediaBlobUrl} controls></audio>
+                                    <video id="myVideo" ref={node => this.videoNode = node} className="video-js vjs-default-skin" playsInline></video>
                             </div>
                         </Row>
                         <Row>
                             <div className="col-md-4"></div>
                             <div className="col-md-4">
-                                <button className="button-color" onClick={startRecording}>Record</button>
-                                <button className="button-color" onClick={stopRecording}>Stop</button>
-                                <button className="button-color" onClick={Upload}>Upload</button>
                             </div>
                         </Row>
                     </Jumbotron>
                 </Col>
             </Row>
         </Container>
-    )
+        );
+    };
 };
 
-export default Record;
+export default RecordComp;
