f3404d3b1ec08525df050ef30ca449cee6077f97
Andrew McGhie <andrewcmcghie@gmail.com>
2021-01-31 19:43:33 -0500

    Sign up form

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Route, BrowserRouter } from 'react-router-dom';
-
-// import Register from './components/Register';
-// import './styles.css';
-
-// const routes = (
-//   <BrowserRouter>
-//     <Route exact path="/" component={Register} />
-//   </BrowserRouter>
-// );
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Log from "../components/Login/index";
+import Log from '../components/Login/index';
 import { Col, Row, Container } from '../components/Grid';
 import API from '../utils/API';
 
 function Login() {
-    return (
-        <Container>
-        <Log />
-        </Container>
-    )
+  return (
+    <Container>
+      <Log />
+    </Container>
+  );
 }
 
 export default Login;
