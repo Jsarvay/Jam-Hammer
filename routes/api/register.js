dbc5c9f293991bedfe086d2c0ad319e9febde8df
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-02-04 21:16:37 -0500

    stuff

--- routes/api/register.js
+++ routes/api/register.js
@@ -35,11 +35,15 @@ function registerUser(req, res) {
                     result: "success"
                 };
                 res.json(sendBack);
+            } else {
+                console.log("error authenticating user: " + username);
+                var sendBack = { result: "fail", reason: "bad username or password" };
+                res.json(sendBack);
             }
             // Value 'result' is set to false. The user could not be authenticated since the user is not active
         });
     });
-}
+} 
 
 
 //  '/api/register'
