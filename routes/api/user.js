84c523e9a694f3a3975321b18bfe47ad1b3d1488
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-02-06 13:53:51 -0500

    Hello world

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,7 +7,6 @@ const userModel = require("../../models/users");
 // Matches with "/api/users"
 router.route('/').get(userController.findAll);
 
-
 // Matches with "/api/users/:id"
 router
   .route('/:id')
