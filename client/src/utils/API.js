7728c743d944ec91ccbaf11698203da41de0afa9
Kilroy <jordans1@vt.edu>
2021-02-06 11:06:11 -0500

    Added login API call and header to upload

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,5 +15,8 @@ export default {
   },
   registerUser: function(userData) {
     return axios.post("/api/register", userData);
-  }  
+  },
+  loginUser: function(loginData) {
+    return axios.post("/api/login", loginData);
+  }
 };
