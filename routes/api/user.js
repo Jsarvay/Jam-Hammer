d4f8227667ed1e68c8e49cec9f6665a093a56c44
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-02-07 19:49:28 -0500

    added get current user api route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const userController = require('../../controllers/userController');
 const userModel = require("../../models/users");
+const userAuth = require("../../utils/userAuth");
 
+function getCurrentUser(req, res){
+  userAuth.authUser(req).then((user)=>{
+    res.json(user);
+  }).catch((err)=>{
+    res.json(err);
+  })
+}
 
 
 // Matches with "/api/users"
-router.route('/').get(userController.findAll);
+router.route('/').get(getCurrentUser);
 
 // Matches with "/api/users/:id"
 router
