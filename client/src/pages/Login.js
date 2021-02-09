57d3e794c9f8df459aafca5af6f090d2e6b51307
Andrew McGhie <andrewcmcghie@gmail.com>
2021-02-08 20:41:29 -0500

    fixed navbar added logo component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Log from '../components/Login/index';
 import { Col, Row, Container } from '../components/Grid';
+import Logo from '../components/Logo';
 import API from '../utils/API';
+import { Fragment } from 'react';
 
 function Login() {
   return (
-    <Container>
-      <Log />
-    </Container>
+    <Fragment>
+      <Logo />
+      <Container>
+        <Log />
+      </Container>
+    </Fragment>
   );
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,13 +1,19 @@
-import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import Searching from "../components/Searching/index";
+import React from 'react';
+import { Col, Row, Container } from '../components/Grid';
+import Nav from '../components/Nav';
+import Searching from '../components/Searching/index';
+import { Fragment } from 'react';
 
 function Search() {
-    return (
-        <Container>
+  return (
+    <Fragment>
+      <Nav />
+      <Container>
+        <Nav />
         <Searching />
-        </Container>
-    )
+      </Container>
+    </Fragment>
+  );
 }
 
 export default Search;
