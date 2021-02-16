7e18f28aad03be853e2a4d0df2f8f0c5113b14e7
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-02-15 21:03:56 -0500

    Pictures upload to S3

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,39 +3,73 @@ const userController = require('../../controllers/userController');
 const userModel = require("../../models/users");
 const userAuth = require("../../utils/userAuth");
 const jimp = require("jimp");
+const fs = require("fs");
+var path = require("path");
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 
-function getCurrentUser(req, res){
-  userAuth.authUser(req).then((user)=>{
+
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1"
+});
+
+
+function getCurrentUser(req, res) {
+  userAuth.authUser(req).then((user) => {
     userModel.findById(user._id).populate('songs').then((userwithSongs) => {
       res.json(userwithSongs);
-    }).catch((err)=>{
+    }).catch((err) => {
       res.json(err);
     })
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   })
 };
 
-function updateProfilePicture(req, res){
-  var user = authUtil.authUser(req).then((user) => {
+function updateProfilePicture(req, res) {
+  var user = userAuth.authUser(req).then((user) => {
     console.log(req.body);
     console.log(req.files);
-    var file = uuidv4() + req.files;
+    var uuidName = uuidv4();
+    var file = uuidName + req.files.picture.name.substring(req.files.picture.name.lastIndexOf("."));
     var filePath = path.join(process.env.SAMPLE_TEMP_DIR, file);
-    fs.writeFileSync(filePath, req.files.data.data);
+    fs.writeFileSync(filePath, req.files.picture.data);
     console.log("Saved to " + filePath);
+    jimp.read(filePath, (err, pic) => {
+      if (err) {
+        res.json({ "result": "fail", "reason": "failed to load file" });
+      }
+      var fixedName = uuidName + "_fixed.png";
+      var fixedPath = path.join(process.env.SAMPLE_TEMP_DIR, fixedName);
+      pic.resize(512, 512).writeAsync(fixedPath).then((jimp) => {
 
-    var uploadParams = { Bucket: process.env.S3_BUCKET, Key: '', Body: '' };
-    var fileStream = fs.createReadStream(filePath);
-    fileStream.on('error', function (err) {
-      console.log('File Error', err);
+        var uploadParams = { Bucket: process.env.S3_BUCKET, Key: '', Body: '' };
+        var fileStream = fs.createReadStream(fixedPath);
+        fileStream.on('error', function (err) {
+          console.log('File Error', err);
+        });
+        uploadParams.Body = fileStream;
+        uploadParams.Key = "ProfilePictures/" + path.basename(uuidName + ".png");
+
+        // call S3 to retrieve upload file to specified bucket
+        s3.upload(uploadParams, function (err, data) {
+          if (err) {
+            console.log("Error", err);
+            res.json({ "result": "fail", "reason": "failed to upload to S3" });
+          } if (data) {
+            console.log("Upload Success", data.Location);
+            userModel.updateOne({ _id: user._id }, { profilePicture: uuidName }).then((doc) => {
+              console.log(doc);
+              res.json(doc);
+            })
+          }
+        });
+      });
+
     });
-    uploadParams.Body = fileStream;
-    uploadParams.Key = "Profile/" + path.basename(file);
-
-    // call S3 to retrieve upload file to specified bucket
-    // s3.upload(uploadParams, function (err, data) {
-
   });
 }
 
