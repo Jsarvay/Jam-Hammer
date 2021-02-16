e48898b66f70fbc02cc42f2fb6a6606ca8699e5b
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2021-02-15 19:25:07 -0500

    stuff

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userController = require('../../controllers/userController');
 const userModel = require("../../models/users");
 const userAuth = require("../../utils/userAuth");
+const jimp = require("jimp");
 
 function getCurrentUser(req, res){
   userAuth.authUser(req).then((user)=>{
@@ -13,6 +14,29 @@ function getCurrentUser(req, res){
   }).catch((err)=>{
     res.json(err);
   })
+};
+
+function updateProfilePicture(req, res){
+  var user = authUtil.authUser(req).then((user) => {
+    console.log(req.body);
+    console.log(req.files);
+    var file = uuidv4() + req.files;
+    var filePath = path.join(process.env.SAMPLE_TEMP_DIR, file);
+    fs.writeFileSync(filePath, req.files.data.data);
+    console.log("Saved to " + filePath);
+
+    var uploadParams = { Bucket: process.env.S3_BUCKET, Key: '', Body: '' };
+    var fileStream = fs.createReadStream(filePath);
+    fileStream.on('error', function (err) {
+      console.log('File Error', err);
+    });
+    uploadParams.Body = fileStream;
+    uploadParams.Key = "Profile/" + path.basename(file);
+
+    // call S3 to retrieve upload file to specified bucket
+    // s3.upload(uploadParams, function (err, data) {
+
+  });
 }
 
 
@@ -25,5 +49,7 @@ router
   .get(userController.findById)
   .put(userController.update)
   .delete(userController.remove);
+
+router.route("/profile").post(updateProfilePicture);
 
 module.exports = router;
